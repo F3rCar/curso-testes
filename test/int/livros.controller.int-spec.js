a08202fb3e02f7f8454bbeed1c16b895422211ae
Thiago Martins <thiago_valentim1@hotmail.com>
2026-04-02 11:20:40 -0300

    test: reimplementa testes da livros controller

--- test/int/livros.controller.int-spec.js
+++ test/int/livros.controller.int-spec.js
@@ -1,70 +1,35 @@
-import test, { after, before, describe, mock } from 'node:test';
-import conexao from '#db/singleton-connection.js';
 import { LivrosController } from '#controllers/livros.controller.js';
-import { limparBanco } from '#src/commands/limpar-banco.command.js';
-import assert from 'node:assert';
+import test, { after, describe, mock } from 'node:test';
+import conexao from '#db/singleton-connection.js';
 import { criarLivro } from '#test/factories/livro.factory.js';
+import { assertMock } from '#test/utils/mock.assertions.js';
 
 describe('LivrosController', () => {
-  before(async () => {
-    await limparBanco();
-  });
+  const sut = new LivrosController(conexao);
 
   after(async () => {
     await conexao.destroy();
   });
 
-  const controller = new LivrosController(conexao);
-  describe('buscarLivroPorId', () => {
-    test('deve retornar 404 para um livro inexistente', async () => {
-      const req = {
-        params: {
-          id: 9999,
-        },
-      };
-      const res = {
-        status: mock.fn(() => res),
-        json: mock.fn(() => res),
-      };
-
-      await controller.buscarLivroPorId(req, res);
-
-      // Precisamos verificar se o status foi chamado com 404 e o metodo json foi chamado com a mensagem correta
-      const chamadasStatus = res.status.mock.calls;
-      assert.strictEqual(res.status.mock.callCount(), 1);
-      assert.strictEqual(chamadasStatus[0].arguments[0], 404);
-
-      const chamadasJson = res.json.mock.calls;
-      assert.strictEqual(res.json.mock.callCount(), 1);
-      assert.deepStrictEqual(chamadasJson[0].arguments[0], {
-        message: 'Livro não encontrado',
-      });
-    });
-    test('deve retornar um livro existente', async () => {
+  describe('listarLivros', () => {
+    test('Retorna uma lista de livros', async () => {
       // Arrange
-      const livro = await criarLivro({ titulo: 'Livro Existente' });
-
-      const req = {
-        params: {
-          id: livro.id,
-        },
-      };
-      const res = {
-        status: mock.fn(() => res),
-        send: mock.fn(() => res),
+      const livro = await criarLivro({ titulo: 'Livro Teste' });
+      const req = {};
+      const resSpy = {
+        status: mock.fn(() => resSpy),
+        json: mock.fn(() => resSpy),
+        send: mock.fn(() => resSpy),
       };
 
       // Act
-      await controller.buscarLivroPorId(req, res);
+      await sut.listarLivros(req, resSpy);
 
-      // Assert
-      const chamadasStatus = res.status.mock.calls;
-      const chamadasSend = res.send.mock.calls;
-
-      assert.strictEqual(chamadasStatus[0].arguments[0], 200);
-      assert.deepStrictEqual(chamadasSend[0].arguments[0], livro);
+      // Arrange
+      assertMock(resSpy.status).wasCalledWith(200);
+      assertMock(resSpy.send).wasCalledWith([livro]);
     });
   });
-  describe('listarLivros', () => {});
+  describe('buscarLivroPorId', () => {});
   describe('cadastrarLivro', () => {});
 });
